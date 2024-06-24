a7e376d987b71af3c60625f3883be44338a80376
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-06-24 18:59:49 +0300

    Add action for app router

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -4,7 +4,7 @@ import Home from '../ui/Home';
 import Menu, { loader as menuLoader } from '../features/menu/Menu';
 import Cart from '../features/cart/Cart';
 import Order, { loader as orderLoader } from '../features/order/Order';
-import CreateOrder from '../features/order/CreateOrder';
+import CreateOrder, { action as createOrderAction } from '../features/order/CreateOrder';
 import AppLayout from '../ui/AppLayout';
 import Error from '../ui/Error';
 
@@ -40,6 +40,7 @@ export const router = createBrowserRouter([
       {
         path: '/order/new',
         element: <CreateOrder />,
+        action: createOrderAction,
       },
     ],
   },
