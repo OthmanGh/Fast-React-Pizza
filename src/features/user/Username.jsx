3059ed96c41b61e83008b0ad94a946b88904e35f
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-02 20:39:22 +0300

    Starting project from v2

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,15 +1,5 @@
-import { useSelector } from 'react-redux';
-
 function Username() {
-  const username = useSelector((state) => state.user.username);
-  console.log(username);
-
-  if (!username) return null;
-
-  console.log(username);
-  return (
-    <div className="hidden text-sm font-semibold md:block">{username}</div>
-  );
+  return <div className="hidden text-sm font-semibold md:block">Jonas</div>;
 }
 
 export default Username;
